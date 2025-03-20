a64b4c9c3e8f29e80e2b568667d54e1bea26b0a0
Tshering-ongmo <07230082.sherubtse@rub.edu.bt>
2025-03-20 20:47:09 +0600

    Practical 2

--- app.js
+++ app.js
@@ -1,27 +1,59 @@
 // Import required modules
- const express = require('express');
- const path = require('path');
- const dotenv = require('dotenv')
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv')
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const db = require('./config/db');  //Import database connection
+const authRoutes = require('./routes/authRoutes');
 
- //load environment variables
- dotenv.config();
+//load environment variables
+dotenv.config();
 
- // Initialize Express app
- const app = express();
- const PORT = process.env.PORT || 3000;
+// Initialize Express app
+const app = express();
+const PORT = process.env.PORT || 3000;
 
- // Set view engine to EJS
- app.set('view engine', 'ejs');
- app.set('views', path.join(__dirname,'views'));
+// Set view engine to EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
 
- // Serve static files
- app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
- // Import routes
- const indexRoutes = require('./routes/index');
- app.use('/', indexRoutes);
+//Middle ware
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true}));
 
- // Start server
- app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+//Session management
+app.use(session({
+    secret: 'Tshering',
+    resave: false,
+    saveUninitialized:true,
+    cookie:{secure:false}
+}));
+
+// Import and use routes
+const indexRoutes = require('./routes/index');
+app.use('/', indexRoutes);
+app.use('/auth', authRoutes);
+
+//Database test route
+app.get('/db-test', async (req,res)=>{
+    try{
+        const result = await db.one('SELECT NOW() AS current_time');
+        res.json({
+            message: 'Database connected successfully',
+            time: result.current_time,
+        });
+    }catch (err){
+        res
+            .status(500)
+            .json({error: 'Database connection failed', details: err.message});
+        }
+    });
+
+// Start server
+app.listen(PORT, () => {
+console.log(`Server is running on http://localhost:${PORT}`);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
- const router = express.Router();
- // Render the landing page
- router.get('/', (req, res) => {
- res.render('index', { title: 'Online Book Store' });
- });
+const router = express.Router();
+// Render the landing page
+router.get('/', (req, res) => {
+res.render('index', { title: 'Online Book Store' });
+});
 
- module.exports = router;
+module.exports = router;
  
