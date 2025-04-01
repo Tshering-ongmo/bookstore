8f61102d19bd1f10b00229cfec740b600029348b
Tshering-ongmo <07230082.sherubtse@rub.edu.bt>
2025-04-01 19:40:48 +0600

    Practical 3

--- config/db.js
+++ config/db.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const db = pgp({
     host: process.env.DB_HOST || 'localhost',
     port: 5432,
-    database: process.env.DB_NAME || 'bookstore_db',
+    database: process.env.DB_NAME || 'online_bookstore_db',
     user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASS || 'yourpassword'
+    password: process.env.DB_PASS || 'your_password'
 });
  
 module.exports = db;
